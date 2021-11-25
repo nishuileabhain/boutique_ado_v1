2ad98f133bbff6d9e75332603ea5c74337587a01
nishuileabhain <nishuileabhain@gmail.com>
2021-11-25 10:11:29 +0000

    add scroll to top of page function

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
+
 from .models import Product, Category
 
 # Create your views here.
@@ -10,7 +12,7 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
-    category = None
+    categories = None
     sort = None
     direction = None
 
@@ -23,29 +25,33 @@ def all_products(request):
                 products = products.annotate(lower_name=Lower('name'))
             if sortkey == 'category':
                 sortkey = 'category__name'
-                
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
-
-        if 'q'in request.GET:
+        if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "you didn't enter any search criteria")
+                messages.error(request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-    
+            
             queries = Q(name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
+        'search_term': query,
+        'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
@@ -56,13 +62,8 @@ def product_detail(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
 
-    current_sorting = f'{sort}_{direction}'
-
     context = {
         'product': product,
-        'search_term': query,
-        'current_categories': categories,
-        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/product_detail.html', context)
